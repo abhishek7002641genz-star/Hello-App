e55d1bb23e98c83ce4934ae23d51e46b5e6340af
abhishek7002641genz-star <abhishek7002641genz@gmail.com>
2026-03-26 09:07:37 +0530

    Added UC4 code

---
+++ O5UC4.java
@@ -0,0 +1,19 @@
+public class O5UC4 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+
+            System.out.println("Hello, " + names.toString() + "!");
+        }
+    }
+}
