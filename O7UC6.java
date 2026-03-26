1bab34a0933db84a4408e357aa39d6c41cb5f8f1
abhishek7002641genz-star <abhishek7002641genz@gmail.com>
2026-03-26 09:11:25 +0530

    Added UC6 code

---
+++ O7UC6.java
@@ -0,0 +1,17 @@
+public class O7UC6 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+
+            for (String name : args) {
+                names.append(name).append(", ");
+            }
+
+            String result = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello, " + result + "!");
+        }
+    }
+}
