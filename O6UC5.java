08ea94ce6518ad67dd13d5b57319dc94b5509016
abhishek7002641genz-star <abhishek7002641genz@gmail.com>
2026-03-26 09:09:47 +0530

    Added UC5 code

---
+++ O6UC5.java
@@ -0,0 +1,18 @@
+public class O6UC5 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+
+            for (String name : args) {
+                names.append(name).append(", ");
+            }
+
+            // Remove last ", "
+            names.setLength(names.length() - 2);
+
+            System.out.println("Hello, " + names.toString() + "!");
+        }
+    }
+}
